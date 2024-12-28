3b30ca4c77040a594c8eaa09dc8ab259cd98718f
JonathanLu2005 <jonathanlu2005@yahoo.com>
2024-12-28 22:33:02 +0000

    Hello world code

---
+++ 1-FirstProgram.cpp
@@ -0,0 +1,10 @@
+// Library to print info to console, namespace get access to standard library
+#include <iostream>
+using namespace std;
+
+// Function
+int main() {
+    cout << "Hello!";
+    return 0;
+}
+

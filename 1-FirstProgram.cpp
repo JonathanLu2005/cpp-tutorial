377d73156dd80110d14737f11964db70c272ab51
JonathanLu2005 <jonathanlu2005@yahoo.com>
2024-12-28 22:43:17 +0000

    namespaces and cout

--- 1-FirstProgram.cpp
+++ 1-FirstProgram.cpp
@@ -1,10 +1,15 @@
 // Library to print info to console, namespace get access to standard library
 #include <iostream>
+// Specify namespace so we can use cout, namespace are areas of code, or specify in code e.g std::cout, so we can get cout
 using namespace std;
 
-// Function
+// Function, entry point of cpp program, ran first, specifically need to be main to start
 int main() {
-    cout << "Hello!";
+    // << stream insertion operator, used with cout, to provide text
+    // Can use many << to include more
+    // use endl or \n for white space
+    cout << "Hello!" << "Jonathan" << endl << endl;
+    cout << "Hello!" << "Tutorial \n";
     return 0;
 }
 

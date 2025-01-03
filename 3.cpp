573f777cafe2c81bb122ae666b26b7ba3d055f98
JonathanLu2005 <jonathanlu2005@yahoo.com>
2025-01-03 16:18:39 +0000

    data types and variables 3

---
+++ 3.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+using namespace std;
+
+int main() {
+    // all data types available
+    int x = 20;
+    cout << x << endl;
+
+    string y = "jonathan";
+    cout << y << endl;
+
+    float z = 7.89;
+    cout << z << endl;
+
+    bool a = true;
+    cout << a << endl;
+
+    char b = 'b';
+    cout << b << endl;
+
+    // can declare then use later
+    int c, d;
+    c = 20;
+    d = 25;
+    cout << c << d;
+}
+
